05ba589dc2c5a5283ed8c795b1701e7292226b6b
WMCS50 <walid.muslih@gmail.com>
2024-01-04 23:34:17 -0700

    10.3-10.10

--- rate-repository-app/src/components/RepositoryItem.jsx
+++ rate-repository-app/src/components/RepositoryItem.jsx
@@ -8,7 +8,105 @@ const styles = StyleSheet.create({
     backgroundColor: theme.colors.mainBackground,
     padding: 20,
     borderBottomWidth: 10,
-    borderBottomColor: '#f0f0f0',
+    borderBottomColor: theme.colors.borderColor,
+  },
+  
+  headlineInfoContainer: {
+    flexDirection: 'row',
+  },
+
+  headlineImage: {
+    width: 75,
+    height: 75,
+    borderRadius: theme.borderRadius.normal,
+    marginRight: 20,
+  },
+
+  headlineTextContainer: {
+    flex: 1,
+    flexDirection: 'column',
+  },
+
+  countsContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    marginTop: 8,
+  },
+  
+  count: {
+    textAlign: 'center',
+    flex: 1,
+  }, 
+  
+  languageContainer: {
+    marginTop: 4,
+    alignItems: 'flex-start',
+  },
+  
+  language: {
+    backgroundColor: theme.colors.primary,
+    color: theme.colors.textTertiary,
+    borderRadius: theme.borderRadius.normal,
+    paddingVertical: 5,
+    paddingHorizontal: 5,
+  },
+});
+
+const formatCount = (count) => {
+  if (count >= 1000 ) {
+    return `${(count / 1000).toFixed(1)}k`;
+  }
+  return count.toString();
+}
+
+const RepositoryItem = ({item}) => 
+    <View style={styles.container}>
+      <View style={styles.headlineInfoContainer}>
+        <Image 
+          style={styles.headlineImage}
+          source={{ uri: item.ownerAvatarUrl }} />  
+        <View style={styles.headlineTextContainer}>
+          <Text fontWeight='bold' fontSize='subheading'>{item.fullName}</Text>
+          <Text color='textSecondary'>{item.description}</Text>
+          <View style={styles.languageContainer}>
+            <Text style={styles.language}>{item.language}</Text>
+          </View>
+        </View>
+      </View>
+      <View style={styles.countsContainer}>
+        <View style={styles.count}>
+          <Text style={{ textAlign: 'center' }} fontWeight='bold'>{formatCount(item.stargazersCount)}</Text>
+          <Text style={{ textAlign: 'center' }}>Stars</Text>  
+        </View>
+        <View style={styles.count}>
+          <Text style={{ textAlign: 'center' }} fontWeight='bold'>{formatCount(item.forksCount)}</Text>
+          <Text style={{ textAlign: 'center' }}>Forks</Text>  
+        </View>
+        <View style={styles.count}>
+          <Text style={{ textAlign: 'center' }} fontWeight='bold'>{formatCount(item.reviewCount)}</Text>
+          <Text style={{ textAlign: 'center' }}>Reviews</Text>  
+        </View>
+        <View style={styles.count}>
+          <Text style={{ textAlign: 'center' }} fontWeight='bold'>{formatCount(item.ratingAverage)}</Text>
+          <Text style={{ textAlign: 'center' }}>Rating</Text>  
+        </View>
+      </View>
+    </View>
+
+export default RepositoryItem;
+
+/*
+import React from 'react';
+import { View, StyleSheet, Image } from 'react-native';
+import Text from './Text';
+import theme from '../theme';
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: theme.colors.mainBackground,
+    padding: 20,
+    borderBottomWidth: 10,
+    borderBottomColor: theme.colors.borderColor,
   },
   
   headlineInfoContainer: {
@@ -94,3 +192,4 @@ const RepositoryItem = ({item}) =>
     </View>
 
 export default RepositoryItem;
+*/
--- rate-repository-app/src/components/SignIn.jsx
+++ rate-repository-app/src/components/SignIn.jsx
@@ -1,8 +1,65 @@
 import Text from './Text';
+import { TextInput, Pressable, View, StyleSheet } from 'react-native';
+import { Formik, useField } from 'formik';
+import FormikTextInput from './FormikTextInput';
+import theme from '../theme';
+import * as yup from 'yup';
+
+const initialValues = {
+  username: '',
+  password: '',
+};
+
+const styles = StyleSheet.create({
+  button: {
+    backgroundColor: theme.colors.primary,
+    borderRadius: theme.borderRadius.normal,
+    paddingVertical: 20,
+    margin: 20,
+  },
+  buttonText: {
+    color: theme.colors.textTertiary,
+    textAlign: 'center',
+  }
+});
+
+const validationSchema = yup.object().shape({
+  username: yup
+    .string()
+    .required('Username is required'),
+  password: yup
+    .string()
+    .required('Password is required'),
+})
+
+const SignInForm = ({ onSubmit }) => {
+  return (
+  <View>
+    <FormikTextInput name='username' placeholder='Username' />
+    <FormikTextInput name='password' placeholder='Password' secureTextEntry={true}/>
+    <Pressable onPress={onSubmit} style={styles.button}>
+      <Text style={styles.buttonText}>Sign in</Text>
+    </Pressable>
+  </View>
+  );
+};
 
 const SignIn = () => {
-  console.log('sign in')
-  return (<Text>The sign-in view</Text>);
-};
+  const onSubmit = values => {
+    const username = values.username;
+    const password = values.password;
+    console.log(username);
+    console.log(password);
+  }
+  return (
+    <Formik 
+      initialValues={initialValues} 
+      onSubmit={onSubmit}
+      validationSchema={validationSchema}
+    >
+      {({ handleSubmit }) => <SignInForm onSubmit={handleSubmit} />}
+    </Formik>
+  )
+}
 
 export default SignIn;
